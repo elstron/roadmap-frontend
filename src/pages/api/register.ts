2a3a83b643e6a29977feea5e658a1cb986041e38
ElStron <luisstron45@gmail.com>
2025-08-23 12:38:00 -0600

    log

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -6,6 +6,6 @@ const userRepository = new createUserService(new UserRepository());
 const userController = new UserController(userRepository);
 
 const env = import.meta.env;
-
+console.log(env.JWT_SECRET);
 export const GET: APIRoute = (context: AstroSharedContext) => userController.getUserById(context);
 export const POST: APIRoute = (context: AstroSharedContext) =>  userController.register(context, env);
